7df69d9d0825bcaec596bafc15f9a3b5a5e8f14b
mgoe <cram-4991@hotmail.de>
2017-10-16 00:19:34 +0200

    Abgabe: 15.10.17; 1 c) ohne subtract 1 d) mit Rect (stand aber nicht in der Aufgabenstellung) 1 d) random patch kopie nicht genau der image patch

--- sheet00/sheet00.cpp
+++ sheet00/sheet00.cpp
@@ -7,37 +7,120 @@ using namespace std;
 using namespace cv;
 
 void readImage(const char* file, Mat& result) {
-    result = imread("images/bonn.png", CV_LOAD_IMAGE_COLOR);
+	result = imread(file, CV_LOAD_IMAGE_COLOR);
 }
 
 void display(const char* windowTitle, Mat& img) {
-    namedWindow(windowTitle, WINDOW_AUTOSIZE);
-    imshow(windowTitle, img);
-    waitKey(0);
+	namedWindow(windowTitle, WINDOW_AUTOSIZE);
+	imshow(windowTitle, img);
+	waitKey(0);
 }
 
 void convertToGrayImg(Mat& img, Mat& result) {
-    cvtColor(img, result, CV_BGR2GRAY);
+	cvtColor(img, result, CV_BGR2GRAY);
 }
 
 void subtractIntensityImage(Mat& bgrImg, Mat& grayImg, Mat& result) {
-    //TODO: implement your solution here
+	//bgrImg - 0.5*grayImg
+	result = bgrImg.clone();
+	for(int r=0; r < bgrImg.rows; r++) {
+		for(int c=0; c < bgrImg.cols; c++) {
+			result.at<Vec3b>(r,c)[0] = std::max( bgrImg.at<Vec3b>(r,c)[0] - 0.5*grayImg.at<uchar>(r,c),0.); //blue
+			result.at<Vec3b>(r,c)[1] = std::max( bgrImg.at<Vec3b>(r,c)[1] - 0.5*grayImg.at<uchar>(r,c),0.); //green
+			result.at<Vec3b>(r,c)[2] = std::max( bgrImg.at<Vec3b>(r,c)[2] - 0.5*grayImg.at<uchar>(r,c),0.);	//red		
+		}
+	}
 }
 
 void pixelwiseSubtraction(Mat& bgrImg, Mat& grayImg, Mat& result) {
-    //TODO: implement your solution here
+
+	int channels = bgrImg.channels();
+
+	int nRows = bgrImg.rows;
+	int nCols = bgrImg.cols * channels;
+
+	result = bgrImg.clone();	
+
+	if (bgrImg.isContinuous()) {
+		nCols *= nRows;
+		nRows = 1;
+	}
+
+	for( int i = 0; i < nRows; i++) {
+		int cols_grayImg = 0;
+		for (int j = 0; j < nCols; j=j+3) {
+			result.ptr<uchar>(i)[j] = std::max(bgrImg.ptr<uchar>(i)[j]-0.5*grayImg.ptr<uchar>(i)[cols_grayImg],0.);
+			result.ptr<uchar>(i)[j+1] = std::max(bgrImg.ptr<uchar>(i)[j+1]-0.5*grayImg.ptr<uchar>(i)[cols_grayImg],0.);
+			result.ptr<uchar>(i)[j+2] = std::max(bgrImg.ptr<uchar>(i)[j+2]-0.5*grayImg.ptr<uchar>(i)[cols_grayImg],0.);
+			cols_grayImg++;
+		}
+	}
 }
 
 void extractPatch(Mat& img, Mat& result) {
-    //TODO: implement your solution here
+	Rect myROI((int) img.cols/2-8, (int) img.rows/2-8, 16, 16);
+	result = img.clone();
+	result = result(myROI);
 }
 
 void copyPatchToRandomLocation(Mat& img, Mat& patch, Mat& result) {
-    //TODO: implement your solution here
+	unsigned int random1 = std::rand()%(img.rows-16);
+	unsigned int random2 = std::rand()%(img.cols-16);
+
+	result = img.clone();	
+	int i,j = 0;
+	for(int r=random1; r < random1+16; r++) {
+		for(int c=random2; c < random2+16; c++) {
+			result.at<Vec3b>(r,c)[0] = patch.at<Vec3b>(i,j)[0]; 	//blue
+			result.at<Vec3b>(r,c)[1] = patch.at<Vec3b>(i,j)[1];	//green
+			result.at<Vec3b>(r,c)[2] = patch.at<Vec3b>(i,j)[2];	//red	
+			j++;	
+		}
+		i++;
+	}
+	
 }
 
 void drawRandomRectanglesAndEllipses(Mat& img) {
-    //TODO: implement your solution here
+	for(int i=0; i < 10; i++) {
+		unsigned int random_size_r = std::rand()%100+10;
+		unsigned int random_size_c = std::rand()%100+10;
+		unsigned int random_size_ellipse;
+		unsigned int random1 = std::rand()%img.rows;
+		unsigned int random2 = std::rand()%img.cols;
+		unsigned int random3, random4;
+		if(random1 + random_size_r < img.rows) {
+			random3 = random1+random_size_r;
+		}
+		else {
+			random3 = img.rows;
+		}
+		if(random2 + random_size_c < img.cols) {
+			random4 = random2+random_size_c;
+		}
+		else {
+			random4 = img.cols;
+		}
+		
+		rectangle(
+			img,								// drawn in img
+			Point(random2, random1),					// opposite vertices 
+			Point(random4, random3),		
+			Scalar(255,255,255),						// color white
+			2,								// thickness
+			8,								// lineType
+			0);								// shift
+
+		ellipse( img,
+           		Point( random2, random1 ),
+           		Size( random_size_c, random_size_r ),
+			0,								// angle
+			0,
+			360,
+			Scalar( 255, 255, 255),						// color white
+			2,								// thickness
+			8 );								// lineType
+	}
 }
 
 int main(int argc, char* argv[])
