20b695e0279afefdf2e139d147d51d14d8dfa1e6
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-11-19 15:55:46 +0100

    Propagation scheme

--- Sheet04/src/sheet04.cpp
+++ Sheet04/src/sheet04.cpp
@@ -229,35 +229,44 @@ void drawSnake(       cv::Mat                   img,
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void gradient_descent(cv::Mat& phi, cv::Mat& img_magnitudes) {
-    double tau = 0.01;
-    double epsilon = 0.0001;
+void gradient_descent(cv::Mat& phi, cv::Mat& w, cv::Mat& w_grad_x, cv::Mat& w_grad_y) {
+    double tau = 1;
+    double epsilon = 0.01;
     int sobel_kernel_size = 1;
 
-    cv::Mat phi2;
-    cv::pow(phi, 2, phi2);
+    //cv::Mat w(phi.size(), CV_32FC1);
+    //cv::pow(phi, 2, phi2);
 
     cv::Mat grad_phi_x, grad_phi_y, grad_phi_xx, grad_phi_yy, grad_phi_xy;
-    cv::Mat grad_phi2_x, grad_phi2_y;
+    //cv::Mat grad_phi2_x, grad_phi2_y;
     cv::Sobel(phi, grad_phi_x, CV_32FC1, 1, 0, 3);
     cv::Sobel(phi, grad_phi_y, CV_32FC1, 0, 1, 3);
     cv::Sobel(phi, grad_phi_xx, CV_32FC1, 2, 0, 3);
     cv::Sobel(phi, grad_phi_yy, CV_32FC1, 0, 2, 3);
     cv::Sobel(phi, grad_phi_xy, CV_32FC1, 1, 1, 3);
-    cv::Sobel(phi2, grad_phi2_x, CV_32FC1, 1, 0, sobel_kernel_size);
-    cv::Sobel(phi2, grad_phi2_y, CV_32FC1, 0, 1, sobel_kernel_size);
+    //cv::Sobel(phi2, grad_phi2_x, CV_32FC1, 1, 0, sobel_kernel_size);
+    //cv::Sobel(phi2, grad_phi2_y, CV_32FC1, 0, 1, sobel_kernel_size);
 
-    double add;
-    for (int x=0; x<phi.rows; x++) {
-        for (int y=0; y<phi.cols; y++) {
-            add = 0;
-            add += grad_phi_xx.at<float>(x, y) * grad_phi_y.at<float>(x, y) * grad_phi_y.at<float>(x, y);
-            add -= 2 * grad_phi_x.at<float>(x, y) * grad_phi_y.at<float>(x, y) * grad_phi_xy.at<float>(x, y);
-            add += grad_phi_yy.at<float>(x, y) * grad_phi_x.at<float>(x, y) * grad_phi_x.at<float>(x, y);
-            add /= grad_phi_x.at<float>(x, y) * grad_phi_x.at<float>(x, y) + grad_phi_y.at<float>(x, y)*grad_phi_y.at<float>(x, y) + epsilon;
+    double mean_curv_motion, propagation;
+    for (int x=1; x<phi.rows-1; x++) {
+        for (int y=1; y<phi.cols-1; y++) {
+            mean_curv_motion = 0;
+            propagation = 0;
+            mean_curv_motion += grad_phi_xx.at<float>(x, y) * grad_phi_y.at<float>(x, y) * grad_phi_y.at<float>(x, y);
+            mean_curv_motion -= 2 * grad_phi_x.at<float>(x, y) * grad_phi_y.at<float>(x, y) * grad_phi_xy.at<float>(x, y);
+            mean_curv_motion += grad_phi_yy.at<float>(x, y) * grad_phi_x.at<float>(x, y) * grad_phi_x.at<float>(x, y);
+            mean_curv_motion /= grad_phi_x.at<float>(x, y) * grad_phi_x.at<float>(x, y) + grad_phi_y.at<float>(x, y)*grad_phi_y.at<float>(x, y) + epsilon;
+            mean_curv_motion *= tau * w.at<float>(x, y);
 
-            add *= tau / (img_magnitudes.at<float>(x, y) + 1);
-            phi.at<float>(x, y) += add;
+            double loc_w = w_grad_x.at<float>(x, y);
+            if (loc_w < 0)  propagation += loc_w * (phi.at<float>(x+1, y) - phi.at<float>(x, y));
+            else            propagation += loc_w * (phi.at<float>(x, y) - phi.at<float>(x-1, y));
+
+            loc_w = w_grad_y.at<float>(x, y);
+            if (loc_w < 0)  propagation += loc_w * (phi.at<float>(x, y+1) - phi.at<float>(x, y));
+            else            propagation += loc_w * (phi.at<float>(x, y) - phi.at<float>(x, y-1));
+
+            phi.at<float>(x, y) += mean_curv_motion + propagation;
         }
     }
 
@@ -304,11 +313,23 @@ void levelSetContours( const cv::Mat& img, const cv::Point2f center, const float
     cv::Sobel(img_gray, img_grad_y, CV_32F, 0, 1, 3);
     cv::cartToPolar(img_grad_x, img_grad_y, img_magnitudes, img_angles, false);
 
+    cv::Mat w(phi.size(), CV_32FC1);
+    for (int x = 0; x < img_magnitudes.rows; x++) {
+        for (int y = 0; y < img_magnitudes.cols; y++) {
+            w.at<float>(x, y) = 0.001 / (img_magnitudes.at<float>(x, y) + 1);
+        }
+    }
+    cv::Mat w_grad_x, w_grad_y;
+    cv::Sobel(w, w_grad_x, CV_32FC1, 1, 0, 3);
+    cv::Sobel(w, w_grad_y, CV_32FC1, 0, 1, 3);
+
+
     for (int k = 0; k < 13000; k++) {
-        gradient_descent(phi, img_magnitudes);
+        gradient_descent(phi, w, w_grad_x, w_grad_y);
 
         if (k % 100 == 0) {
             showGray(    phi, "phi", 1 );
+            temp = computeContour( phi, 0.0f);
             showContour( img, temp,  1 );
             std::cout << "k = "<< k << std::endl;
         }
