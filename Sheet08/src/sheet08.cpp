8391647d1e2b70ac29ea8e2d8f621991c1672ac4
Christopher Schmidt <chris@localhost>
2017-12-16 00:30:02 +0100

    Zwischenstand

--- Sheet08/src/sheet08.cpp
+++ Sheet08/src/sheet08.cpp
@@ -8,74 +8,92 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, char* argv[])
-{
-  ///=======================================================///
-  ///                        Task-1                         ///
-  ///=======================================================///
+int main(int argc, char *argv[]) {
+    ///=======================================================///
+    ///                        Task-1                         ///
+    ///=======================================================///
+    int neighbouring_matches = 2;
+    float threshold = 0.4;
 
-  /// read and show images
-  Mat image1 = imread("./images/mountain1.png", IMREAD_COLOR);
-  Mat image2 = imread("./images/mountain2.png", IMREAD_COLOR);
-  imshow("Image-1", image1);
-  imshow("Image-2", image2);
-  waitKey(0);
-  destroyAllWindows();
+    /// read and show images
+    Mat image1 = imread("./images/mountain1.png", IMREAD_COLOR);
+    Mat image2 = imread("./images/mountain2.png", IMREAD_COLOR);
+    imshow("Image-1", image1);
+    imshow("Image-2", image2);
+    waitKey(0);
+    destroyAllWindows();
 
-  /// compute keypoints and descriptors
-  SIFT sift;
+    /// compute keypoints and descriptors
+    SIFT sift;
+    vector<KeyPoint> keypoints1, keypoints2;
+    Mat descriptors1, descriptors2;
+    sift(image1, Mat(), keypoints1, descriptors1, false);
+    sift(image2, Mat(), keypoints2, descriptors2, false);
 
+    /// show keypoints
+    Mat image_keypoints1, image_keypoints2;
+    drawKeypoints(image1, keypoints1, image_keypoints1);
+    drawKeypoints(image2, keypoints2, image_keypoints2);
+    imshow("Keypoints-1", image_keypoints1);
+    imshow("Keypoints-2", image_keypoints2);
 
-
-  /// show keypoints
-
-
-
-  /// compute nearest matches
-  BFMatcher matcher;
-
-
-
-
-  /// filter matches by ratio test
-
-
-
-
-
-  /// determine two-way matches
+    /// compute nearest matches
+    BFMatcher matcher;
+    vector<vector<DMatch>> matches_1to2;
+    matcher.knnMatch(descriptors1, descriptors2, matches_1to2, neighbouring_matches);
 
 
+    /// filter matches by ratio test
+    vector<DMatch> filtered_matches;
+    for ( auto nearest_matches : matches_1to2 ) {
+        float ratio = nearest_matches[0].distance / nearest_matches[1].distance;
+        if (ratio <= threshold) {
+            filtered_matches.push_back(nearest_matches[0]);
+        }
+    }
 
+    cout << "Size keypoints1: " << keypoints1.size() << endl;
+    cout << "Size keypoints2: " << keypoints2.size() << endl;
+    cout << "Size matches: " << matches_1to2.size() << endl;
 
-  /// visualize matching key-points
-
-
-  waitKey(0);
-  destroyAllWindows();
-
-
-  ///=======================================================///
-  ///                        Task-2                         ///
-  ///=======================================================///
-
-  /// Implement RANSAC here
-
+    /// determine two-way matches
+    vector<vector<DMatch>> matches_2to1;
+    matcher.knnMatch(descriptors1, descriptors2, matches_2to1, neighbouring_matches);
 
 
 
 
+    /// visualize matching key-points
+    Mat img_matches;
+    drawMatches(image1, keypoints1, image2, keypoints2, filtered_matches, img_matches);
+    imshow("Matches", img_matches);
 
 
-  ///  Transform and stitch the images here
+    waitKey(0);
+    destroyAllWindows();
+
+
+    ///=======================================================///
+    ///                        Task-2                         ///
+    ///=======================================================///
+
+    /// Implement RANSAC here
 
 
 
 
 
 
-  /// visualize stitched image
-  waitKey(0);
-  destroyAllWindows();
+
+    ///  Transform and stitch the images here
+
+
+
+
+
+
+    /// visualize stitched image
+    waitKey(0);
+    destroyAllWindows();
 
 }
